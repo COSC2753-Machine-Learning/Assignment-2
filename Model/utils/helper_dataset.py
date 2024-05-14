eb8b58885c588df28cdeae13fa483db650d6fe9e
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2024-05-14 08:37:13 +0700

    Update Folder Structure

--- Model/utils/helper_dataset.py
+++ Model/utils/helper_dataset.py
@@ -1,3 +1,6 @@
+import os
+import pandas as pd
+from PIL import Image, UnidentifiedImageError
 import torch
 from torch.utils.data import sampler
 from torchvision import datasets
@@ -6,6 +9,53 @@ from torch.utils.data import SubsetRandomSampler
 from torchvision import transforms
 from torch.utils.data import random_split
 from torchvision.datasets import ImageFolder
+
+def load(from_dir: str) -> pd.DataFrame:
+    if from_dir[-1] != '/' and from_dir[-1] != '\\':
+        from_dir += '/'
+
+    data_dict = {
+        'ImgPath': [],
+        'FileType': [],
+        'Width': [],
+        'Height': [],
+        'Ratio': [],
+        'Mode': [],
+        'Bands': [],
+        'Transparency': [],
+        'Animated': [],
+        'Category': [],
+        'Interior_Style': []
+    }
+
+    for category in os.listdir(from_dir):
+        category_path = os.path.join(from_dir, category)
+        if os.path.isdir(category_path):
+            for style in os.listdir(category_path):
+                style_path = os.path.join(category_path, style)
+                if os.path.isdir(style_path):
+                    for img_name in os.listdir(style_path):
+                        img_path = os.path.join(style_path, img_name)
+                        try:
+                            with Image.open(img_path) as im:
+                                data_dict['ImgPath'].append(f'{category}/{style}/{img_name}')
+                                data_dict['FileType'].append(img_name.split('.')[-1])
+                                data_dict['Width'].append(im.size[0])
+                                data_dict['Height'].append(im.size[1])
+                                data_dict['Ratio'].append(im.size[0] / im.size[1])
+                                data_dict['Mode'].append(im.mode)
+                                data_dict['Bands'].append(' '.join(im.getbands()))
+                                data_dict['Transparency'].append(
+                                    True if 'transparency' in im.info or im.mode in ('RGBA', 'RGBa', 'LA', 'La', 'PA') else False
+                                )
+                                data_dict['Animated'].append(im.is_animated if hasattr(im, 'is_animated') else False)
+                                data_dict['Category'].append(category)
+                                data_dict['Interior_Style'].append(style)
+                        except (UnidentifiedImageError, PermissionError) as e:
+                            print(f"Error processing {img_path}: {e}")
+
+    return pd.DataFrame(data_dict)
+
 
 class UnNormalize(object):
     def __init__(self, mean, std):
